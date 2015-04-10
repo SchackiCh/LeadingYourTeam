eebea40c0e79be2fcee3f200e66707da732c3be0
SchackiCh <christianschackerl@hotmail.com>
2015-04-10 15:22:01 +0200

    leading your team app
    start the money maker

---
+++ LeadingYourTeam/scripts/controllers.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+    angular.module("mainLeader.controllers")
+        .controller('announcement', ['$scope', 'maps', 'storage', function ($scope, maps, storage) {
+            var refresh = function () {
+                //$scope.todos = storage.getAll();
+            }
+
+            $scope.addAnnouncement = function () {
+                var text = $scope.newAnnouncementModel;
+                if (!text) {
+                    return;
+                };
+            }
+        }]);
+})();
